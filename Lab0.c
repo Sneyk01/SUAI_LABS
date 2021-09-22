72a017dfdb0ce7530d60b363a666386e58a89ac3
“Sneyk” <stalcraft007@mail.ru>
2021-09-22 23:36:40 +0300

    Lab0(if)

--- Lab0.c
+++ Lab0.c
@@ -6,12 +6,7 @@ main() {
 	int a = 0, b = 0, c = 0, code = 0;
 	
 	printf("Enter 2 integers: \n");
-	
-	while ((code = scanf("%d%d", &a, &b) != 2 && (b == 0))) {
-		printf("INPUT ERROR! \n");
-		getchar();
-		getchar();
-	}
+	code = scanf("%d%d", &a, &b)
 
 	if ((code == 2) || (b != 0)) {
 		c = a % b;
