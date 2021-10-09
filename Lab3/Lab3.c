9bac7c8b3f6c1867193e188c5e3f945545c15678
“Sneyk” <stalcraft007@mail.ru>
2021-10-09 15:47:47 +0300

    2.0

---
+++ Lab3/Lab3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>;
+#define LenArr 10
+
+void EditArr(int* arrF, int l) {
+	int avInt = 0;
+	for (int i = 0; i < l; i++)	avInt += arrF[i];
+	avInt /= l;
+	printf("\nAverage integer: %d \n", avInt);
+	for (int i = 0; i < l; i++)	if (arrF[i] > avInt) arrF[i] = 0;
+}
+
+main() {
+	int arr[LenArr] = { 0 };
+	char symb = 1;
+	printf("Please enter 10 integers\n");
+	for (int i = 0; i < LenArr; i++) {
+		printf("%d numder:  ", i + 1);
+		while (scanf("%d", &arr[i]) != 1) {
+			while (getchar() != '\n') {}
+			printf("Input Error! Please enter %d numder:  ", i + 1);	
+		}
+	}
+
+	printf("\n\nold mass: \n");
+	for (int i = 0; i < LenArr; i++) printf("%d ", arr[i]);
+
+	EditArr(&arr[0], 10);
+
+	printf("new mass: \n");
+	for (int i = 0; i < LenArr; i++) printf("%d ", arr[i]);
+
+}
