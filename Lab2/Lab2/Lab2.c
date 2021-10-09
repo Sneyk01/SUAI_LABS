93c267459752d818877450d2cfb9e323c336f1cc
“Sneyk” <stalcraft007@mail.ru>
2021-10-09 13:40:12 +0300

    1.5

--- Lab2/Lab2/Lab2.c
+++ Lab2/Lab2/Lab2.c
@@ -2,27 +2,31 @@
 
 
 main() {
-	int i = 0, space = 1, space2 = 0, first = 1;
+	int i = 0, fs = 1, space = 0, firstSS = 1;
 	char a[10000] = "";
 	printf("Print string (max 10000 symb):\n");
 	fgets(&a, 10000, stdin);
-	while (a[i] != 10) {
-		if (a[i] == ' ' || first == 1) {
-			first = 0;
-			i++;
-			space = i;
-			while (a[i] != ' ' && a[i] != 10) {
+	while (a[i] != 0) {
+		if (a[i] == ' ' || a[i] == '\t' || firstSS == 1) {
+			if (a[i] == ' ' || a[i] == '\t')	fs = i + 1;
+			else fs = i;
+			if (firstSS == 0) i++;
+			firstSS = 0;
+			while (a[i] != ' ' && a[i] != '\t' && a[i] != 0 && a[i] != '\n') {
 				i++;
-				space2 = i;
+				space = i;
 			}
 		}
+		
+		if ((space - fs > 0) && (a[space - 1] == 'a' || a[space - 1] == 'e' || a[space - 1] == 'u' || a[space - 1] == 'i' || a[space - 1] == 'o' || a[space - 1] == 'y' || a[space - 1] == 'A' || a[space - 1] == 'E' || a[space - 1] == 'U' || a[space - 1] == 'I' || a[space - 1] == 'O' || a[space - 1] == 'Y')) {
+			for (int f = fs; f < space; f++)	printf("%c", a[f]);
+			printf(" ");
+			space = 0;
+		}
 
-		if ((space2 > 0) && (a[space2 - 1] == 'a' || a[space2 - 1] == 'e' || a[space2 - 1] == 'u' || a[space2 - 1] == 'i' || a[space2 - 1] == 'o' || a[space2 - 1] == 'y' || a[space2 - 1] == 'A' || a[space2 - 1] == 'E' || a[space2 - 1] == 'U' || a[space2 - 1] == 'I' || a[space2 - 1] == 'O' || a[space2 - 1] == 'Y')) {
-			for (int f = space; f <= space2; f++) {
-				printf("%c", a[f]);
-			}
-			//i++;
-			space2 = 0;
+		if (a[i] == '\n') {
+			i++;
+			firstSS = 1;
 		}
 	}
 }
