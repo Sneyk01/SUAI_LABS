88cf656ee47028e6aec3792418de4b3a31ac68a7
“Sneyk” <stalcraft007@mail.ru>
2021-11-06 19:50:25 +0300

    1.8

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -5,73 +5,74 @@ int checkS(char symb) {
     return 0;
 }
 
-double getNum(int bf, int minusMem, double degree, double* numb, char* str, int dotMem) {
+double getNum(int bf, int minusMem, double degree, double numb, char* str, int dotMem, int minus) {
     for (bf; bf > minusMem; bf--) {
         if (bf != dotMem) {
-            *numb += (str[bf] - 48) * degree;
+            numb += (str[bf] - 48) * degree;
             degree *= 10;
         }
     }
-    return *numb * (-1);
+    if (minus == 1) return numb * (-1);
+    return numb;
 }
 
 void checkWord(char *a,int *word, int *iA, double *numb ) {
     int i = *iA;
     if (a[i] == 'o' && a[i + 1] == 'n' && a[i + 2] == 'e') {
         *word = 1;
-        *numb = -1;
-        *iA += 2;
+        *numb = 1;
+        *iA += 3;
     }
     if (a[i] == 't' && a[i + 1] == 'w' && a[i + 2] == 'o' && *word == 0) {
         *word = 1;
-        *numb = -2;
-        *iA += 2;
+        *numb = 2;
+        *iA += 3;
     }
     if (a[i] == 't' && a[i + 1] == 'h' && a[i + 2] == 'r' && a[i + 3] == 'e' && a[i + 4] == 'e' && *word == 0) {
         *word = 1;
-        *numb = -3;
-        *iA += 3;
+        *numb = 3;
+        *iA += 5;
     }
     if (a[i] == 'f' && a[i + 1] == 'o' && a[i + 2] == 'u' && a[i + 3] == 'r' && *word == 0) {
         *word = 1;
-        *numb = -4;
-        *iA += 3;
+        *numb = 4;
+        *iA += 4;
     }
     if (a[i] == 'f' && a[i + 1] == 'i' && a[i + 2] == 'v' && a[i + 3] == 'e' && *word == 0) {
         *word = 1;
-        *numb = -5;
-        *iA += 3;
+        *numb = 5;
+        *iA += 4;
     }
     if (a[i] == 's' && a[i + 1] == 'i' && a[i + 2] == 'x' && *word == 0) {
         *word = 1;
-        *numb = -6;
-        *iA += 2;
+        *numb = 6;
+        *iA += 3;
     }
     if (a[i] == 's' && a[i + 1] == 'e' && a[i + 2] == 'v' && a[i + 3] == 'e' && a[i + 4] == 'n' && *word == 0) {
         *word = 1;
-        *numb = -7;
-        *iA += 4;
+        *numb = 7;
+        *iA += 5;
     }
     if (a[i] == 'e' && a[i + 1] == 'i' && a[i + 2] == 'g' && a[i + 3] == 'h' && a[i + 4] == 't' && *word == 0) {
         *word = 1;
-        *numb = -8;
-        *iA += 4;
+        *numb = 8;
+        *iA += 5;
     }
     if (a[i] == 'n' && a[i + 1] == 'i' && a[i + 2] == 'n' && a[i + 3] == 'e' && *word == 0) {
         *word = 1;
-        *numb = -9;
-        *iA += 3;
+        *numb = 9;
+        *iA += 4;
     }
     if (a[i] == 't' && a[i + 1] == 'e' && a[i + 2] == 'n' && *word == 0) {
         *word = 1;
-        *numb = -10;
-        *iA += 2;
+        *numb = 10;
+        *iA += 3;
     }
 }
 
 int main() {
     printf("*This program supports decimal numbers up to five decimal places, other digits are ignored*\n");
-    int i = 0, minusMem = 0, mem = 0, dot = 0, dotMem = 0, amount = 0, flag = 0, word = 0, intNumb = 0;
+    int i = 0, minusMem = 0, mem = 0, dot = 0, dotMem = 0, amount = 0, flag = 0, word = 0, intNumb = 0, minus = 0;
     double degree = 1, numb = 0;
     char a[10000] = "";
     double nums[10000] = {0};
@@ -80,46 +81,49 @@ int main() {
 
     while (a[i] != 0) {
         if (a[i] == '-') {
-            minusMem = i;
-            i++;
-            checkWord(a, &word, &i, &numb);
-            if (word == 1)
-                i++;
-            while ((checkS(a[i]) == 1 || a[i] == ',' || a[i] == '.') && word == 0) {
-                if (a[i] == ',' || a[i] == '.') {
-                    dotMem = i;
-                    dot++;
-                }
-                mem = i;
-                i++;
-            }
-            //printf("%d", numb);
-            if ((dot < 2 && mem > 0) || word == 1) {
-                if (dot == 0 && word == 0)
-                    numb = getNum(mem, minusMem, degree, &numb,&a[0], 0);
-
-                if (dot == 1 && word == 0) {
-                    if (mem - dotMem > 5) mem = dotMem + 5;
-                    for (int d = 0; d < mem - dotMem; d++)  degree /= 10;
-                    numb = getNum(mem, minusMem, degree, &numb, a, dotMem);
-                }
-
-                for (int checkArray = 0; checkArray < amount; checkArray++) {
-
-                    //printf("%f|%f ||", numb, nums[checkArray]);
-                    if (numb == nums[checkArray]) flag = 1;
-                    //printf(" %d \n", flag);
-                }
-
-                if (flag == 0) {
-                    nums[amount] = numb;
-                    amount++;
-                }
-                else    flag = 0;
-            }
+            minusMem = i++;
+            minus = 1;
+            //i++;
         }
-        else    i++;
+        else {
+            minusMem = i - 1;
+            minus = 0;
+        }
+        checkWord(a, &word, &i, &numb);
+        if (word == 1 && minus == 1) numb *= -1;
+        if (minus == 0 && word == 0 && checkS(a[i]) == 0) {
+            i++;
+            goto exit;
+        }
+        while ((checkS(a[i]) == 1 || a[i] == ',' || a[i] == '.') && word == 0) {
+            if (a[i] == ',' || a[i] == '.') {
+                dotMem = i;
+                dot++;
+            }
+            mem = i++;
+            //i++;
+        }
+        if ((dot < 2 && mem > 0) || word == 1) {
+            if (dot == 0 && word == 0)
+                numb = getNum(mem, minusMem, degree, numb,&a[0], -1, minus);
 
+            if (dot == 1 && word == 0) {
+                if (mem - dotMem > 5) mem = dotMem + 5;
+                for (int d = 0; d < mem - dotMem; d++)  degree /= 10;
+                numb = getNum(mem, minusMem, degree, numb, a, dotMem, minus);
+            }
+
+            for (int checkArray = 0; checkArray < amount; checkArray++)
+                if (numb == nums[checkArray]) flag = 1;
+
+            if (flag == 0)  {
+                nums[amount] = numb;
+                amount++;
+            }
+            else    flag = 0;
+        }
+        //i++;
+        exit:
         degree = 1;
         minusMem = 0;
         dotMem = 0;
