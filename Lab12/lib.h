a14be216233b888dc293a142ea3a2d5f196821ac
“Sneyk” <stalcraft007@mail.ru>
2022-04-20 12:56:39 +0300

    add Lab12

---
+++ Lab12/lib.h
@@ -0,0 +1,23 @@
+//
+// Created by prgle on 04.04.2022.
+//
+
+#ifndef LAB12_LIB_H
+#define LAB12_LIB_H
+
+#include <iostream>
+
+class BitArray {
+private:
+    char *data;
+    size_t size_bit;
+public:
+    BitArray (int size);
+    void setOne (int value, int num);
+    int getOne (int num);
+    void printAll ();
+    ~BitArray();
+
+};
+
+#endif //LAB12_LIB_H
