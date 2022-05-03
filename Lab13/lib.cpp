19d5aa0f69eceb45609d2bc6b02c9024c805a5c9
“Sneyk” <stalcraft007@mail.ru>
2022-05-03 21:21:49 +0300

    added file support

---
+++ Lab13/lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by prgle on 03.05.2022.
+//
+
+#include "lib.h"
---
+++ Lab13/lib.h
@@ -0,0 +1,8 @@
+//
+// Created by prgle on 03.05.2022.
+//
+
+#ifndef LAB13_LIB_H
+#define LAB13_LIB_H
+
+#endif //LAB13_LIB_H
